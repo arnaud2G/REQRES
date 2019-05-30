243f6ca531a5236772c0250cc0c59deb2996193b
Arnaud Verrier <arnaudverrier@macbook-pro-de-arnaud.home>
2019-05-30 10:16:09 +0200

    Create project

---
+++ Arnaud/Arnaud/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Arnaud
+//
+//  Created by Arnaud Verrier on 30/05/2019.
+//  Copyright © 2019 Arnaud Verrier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 
 Sart : 10h12
+
+Create Project : 10h15
